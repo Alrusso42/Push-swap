7ba98bfdc9ac35a7f2a570ed46538efd1db0e6d1
Alrusso42 <alrussoprivate@hotmail.com>
2022-04-19 10:05:12 +0200

    y

--- push_sort_3.c
+++ push_sort_3.c
@@ -6,12 +6,13 @@
 /*   By: alrusso <alrusso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 09:52:37 by alrusso           #+#    #+#             */
-/*   Updated: 2022/04/19 09:55:18 by alrusso          ###   ########.fr       */
+/*   Updated: 2022/04/19 10:05:06 by alrusso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/* Instruction dans le cas où le premier nombre est plus grand que le deuxième */
 void	ft_2(t_list **lst_a, t_list **lst_b)
 {
 	(void) lst_b;
@@ -19,12 +20,14 @@ void	ft_2(t_list **lst_a, t_list **lst_b)
 	ft_sa(lst_a);
 }
 
+/* Instruction dans le cas où le premier nombre est plus petit que le deuxième */
 void	ft_5(t_list **lst_a)
 {
 	ft_sa(lst_a);
 	ft_rra(lst_a);
 }
 
+/* Instruction pour les listes de tailles 3 */
 void	ft_len3(t_list **lst_a, t_list **lst_b)
 {
 	if (((*lst_a)->i < (*lst_a)->next->i)
